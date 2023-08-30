5743f972d9f93af9c1d9dfa878c74b7e4974a1b9
Yaswanth1320 <yashwanth.paravathala@gmail.com>
2023-08-30 19:56:59 +0530

    first commit

---
+++ tictactoe.py
@@ -0,0 +1,108 @@
+import random
+
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-"]
+
+currentPlayer = "X"
+winner = None
+gameRunning = True
+
+# printing the board
+
+
+def printBoard(board):
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print("----------")
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print("----------")
+    print(board[6] + " | " + board[7] + " | " + board[8])
+
+
+# take player input
+def playerInput(board):
+    inp = int(input("Enter a number 1-9 :"))
+    if inp >= 1 and inp <= 9 and board[inp-1] == "-":
+        board[inp-1] = currentPlayer
+    else:
+        print("place occupied")
+
+
+# cheak for win or tie
+def cheakHorizontle(board):
+    global winner
+    if board[0] == board[1] == board[2] and board[1] != "-":
+        winner = board[0]
+        return True
+    elif board[3] == board[4] == board[5] and board[3] != "-":
+        winner = board[3]
+        return True
+    elif board[6] == board[7] == board[8] and board[6] != "-":
+        winner = board[6]
+        return True
+
+
+def cheakRow(board):
+    global winner
+    if board[0] == board[3] == board[6] and board[0] != "-":
+        winner = board[0]
+        return True
+    elif board[1] == board[4] == board[7] and board[1] != "-":
+        winner = board[1]
+        return True
+    elif board[2] == board[5] == board[8] and board[2] != "-":
+        winner = board[2]
+        return True
+
+
+def cheakDiag(board):
+    global winner
+    if board[0] == board[4] == board[8] and board[0] != "-":
+        winner = board[0]
+        return True
+    elif board[2] == board[4] == board[6] and board[2] != "-":
+        winner = board[2]
+        return True
+
+
+def cheakTie(board):
+    if "-" not in board:
+        printBoard(board)
+        print("Its a tie !")
+        gameRunning = False
+
+# switch the player
+
+
+def switchPlayer():
+    global currentPlayer
+    if currentPlayer == "X":
+        currentPlayer = "O"
+    else:
+        currentPlayer = "X"
+
+
+def checkWin():
+    if cheakDiag(board) or cheakHorizontle(board) or cheakRow(board):
+        print(f"The winner is {winner}")
+
+# computer
+
+
+def computer(board):
+    while currentPlayer == "O":
+        position = random.randint(0, 8)
+        if board[position] == "-":
+            board[position] = 'O'
+            switchPlayer()
+
+
+while gameRunning:
+    printBoard(board)
+    playerInput(board)
+    checkWin()
+    cheakTie(board)
+    switchPlayer()
+    computer(board)
+    checkWin()
+    cheakTie(board)
